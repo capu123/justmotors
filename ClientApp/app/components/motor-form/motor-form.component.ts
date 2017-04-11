b75b8a9f1f4e4c38a47a17de9b097f32058a8f88
sashi Kala chhetri <saideep_chhetri@yahoo.com>
2017-04-11 04:37:17 +0100

    added motor-form component

---
+++ ClientApp/app/components/motor-form/motor-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motor-form',
+  templateUrl: './motor-form.component.html',
+  styleUrls: ['./motor-form.component.css']
+})
+export class MotorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
