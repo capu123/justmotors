c13325dfab38d49548c1807e192e67c7126c8753
sashi Kala chhetri <saideep_chhetri@yahoo.com>
2017-05-16 16:12:34 +0100

    building vehicle API for DELETE

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -53,6 +53,9 @@ namespace justmotors.Controllers
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
             
+            if(vehicle == null)
+                return NotFound();
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -60,6 +63,34 @@ namespace justmotors.Controllers
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle =  await context.Vehicles.FindAsync(id);
+
+            if(vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+         // var vehicle =  await context.Vehicles.FindAsync(id);
+            var vehicle =  await context.Vehicles.Include(v =>v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if(vehicle == null)
+                return NotFound();
+            
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
         }
     }
 
