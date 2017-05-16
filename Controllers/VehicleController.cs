c9a3821dfbab730d201ff667e385177e0f638734
sashi Kala chhetri <saideep_chhetri@yahoo.com>
2017-05-16 04:42:46 +0100

    adding vechile to DB

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,16 +1,36 @@
 using justmotors.Models;
+using justmotors.Controllers.Resources;
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using justmotors.Persistence;
+using System.Threading.Tasks;
+using System;
 
 namespace justmotors.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
+        private readonly IMapper mapper;
+        private readonly JustmotorsDbContext context;
+        public VehiclesController(IMapper mapper, JustmotorsDbContext context)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
         [HttpPost]
-       public IActionResult CreateVehicle(Vehicle vehicle)
-       {
-           return Ok(vehicle);
-       }
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
     }
 
 }
